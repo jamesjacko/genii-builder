2e6486ec34a65de849f2b046eac7365459b362f2
Unknown <james.ogge@gmail.com>
2019-03-11 13:11:17 +0000

    Timer working
    fixes and closes #6

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import firebase, { sendData } from './utils/firebase.js';
 import TimedButton from './utils/timedButton.js';
 
 class App extends Component {
-
+  buttonClick(){
+    alert("Hello");
+  }
   render() {
     return (
       <div className="App">
-        <h1>Micro-Visualisation Designer <TimedButton text="Next" /></h1>
+        <h1>Micro-Visualisation Designer <TimedButton text="Next" duration={ 300000 } actions={ { click: this.buttonClick.bind(this) } } /></h1>
         <ParameterSelector />
         <GeneBuilder />
         <Renderer />
